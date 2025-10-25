0d6b08b58af8543bc8be5ad94e524b2dab427027
goodw <wataru01208@gmail.com>
2025-10-25 23:08:05 +0900

    ✨feat:add reset button and color button and doneList

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './main.css';
 
 
 //メイン
 function App() {
 
+  // const [active, setActive] = useState();
+
+  const handleActive=(id,NewStatus)=>{
+    setListArray((p)=>p.map((task)=>
+    task.id===id ?{...task, status:task.status == NewStatus ? "" : NewStatus
+    }:task));
+  };
+
   //入力している情報をいれるもの
   const [form,setListForm]=useState({
     day:"",
     start_time:"",
     end_time:"",
-    text:""
+    text:"",
+    done:false
   });
 
   //最終保存，最終的に内容が保存される配列
-  const [ListArray,setListArray]=useState([]);
+  const [ListArray,setListArray]=useState(()=>{
+
+  const save=localStorage.getItem("ListArray");
+  return save ? JSON.parse(save):[];
+
+});
+
+useEffect(()=>{
+      localStorage.setItem("ListArray",JSON.stringify(ListArray));
+    },[ListArray]);
+   
+
 
   //テキストを選択した際に処理される内容
   const handleChange=(c)=>{
@@ -38,9 +58,12 @@ function App() {
     //入力した内容を保存する配列に追加する
     setListArray([...ListArray,newItem]);
     //入力後リセットする
-    setListForm({text:"",day:"",start_time:"",end_time:""});
+    setListForm({text:"",day:"",start_time:"",end_time:"",done:false});
 
   };
+  
+  
+  
 
   //✔ボタンが押されたときの内容
   const handleDelet=(id)=>{
@@ -49,50 +72,105 @@ function App() {
       setListArray(ListArray.filter((t)=> t.id !== id));
       }
     }
+
+  //完了リスト用のstats(完了済みを保存する場所)
+  const [doneList,setDoneList]=useState(()=>{
+
+    const save=localStorage.getItem("doneList");
+    return save ? JSON.parse(save):[];
+  });
+
+  useEffect(()=>{
+      localStorage.setItem("doneList",JSON.stringify(doneList));
+    },[doneList]);
+   
+  
+  
+  //完了リストに追加する
+  const handleDone=(id)=>{
+
+    if(window.confirm("完了リストに追加しますか？")){
+
+    //選択したタスクを見つける
+    const doneItem=ListArray.find((t)=>t.id===id);
+    if(!doneItem)return;
+
+    //完了リスト用の保存先に，選択したタスクを追加（done状態をtrueにして保存する）
+    setDoneList((s)=>[...s,{...doneItem,done:true}]);
+
+    //選択したものだけをタスクから除外する
+    setListArray((s)=>s.filter((t)=>t.id!==id));
+
+    }
+    };
+
+  
+    const doneListDelet=()=>{
+      if(setDoneList.length===0)return;
+      if(window.confirm("完了タスクを削除しますか？")){
+          setDoneList([]);
+      }
+    }
+
+    const resetbutton=()=>{
+      if(window.confirm("本当にすべて消してもよろしいですか？")){
+        setListArray([]);
+        setDoneList([]);
+      }
+    }
   
     //子
    return(
       <div>
-        <h1 class="menue_bar">Todoリスト（react版）</h1>
+        <h1 className="menue_bar">Todoリスト（react版）</h1>
         <div>
-      <input type="date" name="day" placeholder="日付を入力してください" onChange={handleChange} value={form.day}></input>
-      <input type="time" name="start_time" placeholder="開始時間を入力してください" onChange={handleChange} value={form.start_time}></input>
-      <input type="time" name="end_time" placeholder="終了時間を入力してください" onChange={handleChange} value={form.end_time}></input>
-      <input type="text" name="text" placeholder="予定を入力してください" onChange={handleChange} value={form.text}></input>
-      <button onClick={handleAdd_text}>追加</button>
+      年月日：<input type="date" name="day" placeholder="日付を入力してください" onChange={handleChange} value={form.day} className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"></input>
+      開始時間：<input type="time" name="start_time" placeholder="開始時間を入力してください" onChange={handleChange} value={form.start_time} className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"></input>
+      終了時間：<input type="time" name="end_time" placeholder="終了時間を入力してください" onChange={handleChange} value={form.end_time} className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"></input>
+      予定内容：<input type="text" name="text" placeholder="予定を入力してください" onChange={handleChange} value={form.text} className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"></input>
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleAdd_text}>追加</button>
+      <button onClick={resetbutton} className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >リセット</button>
         </div>
           <div>
             
           </div>
           <ul>
-            {ListArray.map((i,index)=>(  
-            <li key={index}>
-              <button onClick={()=>handleDelet(i.id)}>✔</button>
-              日付：{i.day}　　開始時間：{i.start_time}　　終了時刻：{i.end_time}　　タスク：{i.text}
-            </li>
+            <h1>タスク一覧</h1>
+            {ListArray.map((i)=>(  
+            <li key={i.id} className="mb-2"> 
+            <div className={`block p-4 rounded mb-2 ${
+
+              i.status==="important" 
+              ? "bg-yellow-200" 
+              : i.status==="not-start" 
+              ? "bg-red-200"
+              :i.status==="progress"
+              ? "bg-blue-200"
+              :"bg-white"
+              }`}>
+            <div className="flex items-center space-x-4">
+            
+              <button onClick={()=>handleDone(i.id)} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">✔</span></button>
+              <span>日付：{i.day}</span><span>開始時間：{i.start_time}</span>終了時刻：{i.end_time}<span>タスク：{i.text}</span>
+              <div className="text-right">
+              <button onClick={()=>handleActive(i.id,"important")} className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-yellow-200 dark:hover:bg-yellow-200 dark:focus:ring-yellow-200">☆</button>
+              <button onClick={()=>handleActive(i.id,"not-start")} className="text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">未着手</button>
+              <button onClick={()=>handleActive(i.id,"progress")} className="text-white bg-blue-600 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">進行中</button>
+              </div>
+              </div>
+              </div>
+              </li>
             ))}
+            <span><h1>完了リスト</h1><button onClick={doneListDelet}>完了リスト削除</button></span> 
+            {doneList.filter((t)=>t.done).map((i)=>(
+              <div className="flex items-center space-x-4">
+                <span>日付：{i.day}</span><span>開始時間：{i.start_time}</span>終了時刻：{i.end_time}<span>タスク：{i.text}</span></div>
+            ))}
+             
           </ul>
       </div>  
    );
-  }
-
+  
+ }
 export default App;
-
-// function App() {
-//   return (
-//     <div className="min-h-screen bg-blue-100 flex flex-col items-center justify-center">
-//       <h1 className="text-4xl font-bold text-blue-700 mb-4">
-//         Tailwind動作テスト
-//       </h1>
-//       <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-//         ボタン
-//       </button>
-//       <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600">
-//   追加
-// </button>
-
-//     </div>
-//   );
-// }
-
-// export default App;
